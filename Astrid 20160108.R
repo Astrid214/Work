efa0005254826f7dd5ffb61fbcb90b6acb8e9cf5
Astrid214 <atienza2111@gmail.com>
2017-01-08 15:50:22 -0500

    Update Astrid 20160108.R

--- Astrid 20160108.R
+++ Astrid 20160108.R
@@ -1,31 +1,22 @@
-####R training with Drew##
+#### BUDGETS- PUBLIC FILES ####
 
+library(dplyr)
+library(tidyr)
+library(readxl)
 
 ##FY15##
 FY15_Budget <- read.table("C:/Users/astrid.atienza/Desktop/R/Data/FY15_Budget.txt", sep="\t", header=TRUE, strip.white=TRUE, stringsAsFactors=FALSE)
 FY15_Budget <- rename(FY15_Budget, School_Name=School)
 
-
 ##FY16##
 FY16_Budget <- read.csv("C:/Users/astrid.atienza/Desktop/R/Data/FY16_Budget.csv", strip.white=TRUE, stringsAsFactors=FALSE)
 FY16_Budget <- rename(FY16_Budget, School_Name=School.Name)
 
-
 ##FY17##
-library(readxl)
 FY17_Budget <- read_excel("C:/Users/astrid.atienza/Desktop/R/Data/fy17_initial_allocations.xlsx", sheet="Initial Allocations (2.16.16)", skip=1)
 FY17_Budget <- rename(FY17_Budget, School_Name=School Name)
 
-
 ##sample excel inputs: ?read_excel
-
-library(dplyr)
-library(tidyr)
-
-
-##MERGE FY17 FY16 FY15
-
-"Budget_Data" <-left_join(FY17_Budget,FY16_Budget, by="School_Code")
 
 ###Example 7###
 ID <- c("1","2")
@@ -37,13 +28,9 @@ data_long <- gather(data, "Column_Names","Value", 2:4)
 data_wide <- spread(data_long, Column_Names, Value)
 
 
+##MERGE FY17 FY16 FY15##
 
-###### Task 2 ######
-library(readxl)
-library(dplyr)
-library(tidyr)
-
-##FY15##
+##PREP FILE FOR MERGE##
 FY15_Budget <- read.table("C:/Users/astrid.atienza/Desktop/R/Data/FY15_Budget.txt", sep="\t", header=TRUE, strip.white=TRUE, stringsAsFactors=FALSE)
 FY16_Budget <- read.csv("C:/Users/astrid.atienza/Desktop/R/Data/FY16_Budget.csv", strip.white=TRUE, stringsAsFactors=FALSE)
 FY17_Budget <- read_excel("C:/Users/astrid.atienza/Desktop/R/Data/fy17_initial_allocations.xlsx", sheet="Initial Allocations (2.16.16)", skip=1)
@@ -58,5 +45,8 @@ FY16_Budget <- rename(FY16_Budget, School_Name=School.Name,  FY16_Enrollment=FY1
 
 FY17_Budget <- select(SY17_Budget, School.Code)
 FY17_Budget <- rename(FY17_Budget, School_Name=School.Name,  FY16_Enrollment=FY16.Budgeted.Enrollment, FY16_Total_Budget=Total)
+
+
+"Budget_Data" <-left_join(FY17_Budget,FY16_Budget, by="School_Code")
 
 
